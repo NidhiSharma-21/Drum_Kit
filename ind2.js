6161b8825fa11cb992b97fc2bf197fc64cd72bfb
Nidhi Sharma <nidhi.1502ns@gmail.com>
2024-04-22 13:28:24 +0530

    added

---
+++ ind2.js
@@ -0,0 +1,9 @@
+function BellBoy(name,age,hasworkpermit, languages){
+    this.name = name;
+    this.age=age;
+    this.hasworkpermit=hasworkpermit;
+    this.languages=languages;
+    this.clean=function(){
+        alert("cleaning is in progress");
+    }
+ }
